428766195940eace459ad1267838cc6c0bfcafff
Eyal Arubas <eyalarubas@gmail.com>
2015-01-20 10:13:59 +0800

    fix relative paths

--- app/main.js
+++ app/main.js
@@ -1,25 +1,28 @@
 require.config({
     waitSeconds: 0,
-    baseUrl: "./",
+    baseUrl: "bower_components",
     packages: [{
         name: "jquery",
-        location: "bower_components/jquery/dist",
+        location: "jquery/dist",
         main: "jquery"
+    }, {
+        name: "appRoot",
+        location: ".."
     }],
     map: {
         '*': {
-            text: 'bower_components/requirejs-plugins/lib/text',
-            image: 'bower_components/requirejs-plugins/src/image',
-            json: 'bower_components/requirejs-plugins/src/json',
-            async: 'bower_components/requirejs-plugins/src/async',
-            font: 'bower_components/requirejs-plugins/src/font',
-            goog: 'bower_components/requirejs-plugins/src/goog',
-            noext: 'bower_components/requirejs-plugins/src/noext',
-            mdown: 'bower_components/requirejs-plugins/src/mdown',
-            propertyParser: 'bower_components/requirejs-plugins/src/propertyParser',
-            markdownConverter : 'bower_components/requirejs-plugins/lib/Markdown.Converter',
-            less: 'bower_components/require-less/less',
-            when: 'bower_components/when'
+            text: 'requirejs-plugins/lib/text',
+            image: 'requirejs-plugins/src/image',
+            json: 'requirejs-plugins/src/json',
+            async: 'requirejs-plugins/src/async',
+            font: 'requirejs-plugins/src/font',
+            goog: 'requirejs-plugins/src/goog',
+            noext: 'requirejs-plugins/src/noext',
+            mdown: 'requirejs-plugins/src/mdown',
+            propertyParser: 'requirejs-plugins/src/propertyParser',
+            markdownConverter : 'requirejs-plugins/lib/Markdown.Converter',
+            less: 'require-less/less',
+            when: 'when'
         }
     }
 });
@@ -27,19 +30,21 @@ require.config({
 require([
     "jquery",
     "when/when",
-    "json!config.json",
+    "json!appRoot/config.json",
     "require"
 ], function(jQuery, when, config, localRequire){
     // build splash screen:
-    var splashCtrlPath = config.ctrlPath + "splash/" + "main.js",
-        metadataPath = config.dataPath + "meta.json",
-        layoutPath = config.dataPath + "layout.json";
+    var ctrlBase = "appRoot/" + config.ctrlPath,
+        dataBase = "appRoot/" + config.dataPath,
+        contentBase = "appRoot/" + config.contentPath,
+        splashCtrlPath = ctrlBase + "splash/" + "main",
+        metadataPath = dataBase + "meta.json",
+        layoutPath = dataBase + "layout.json";
     localRequire([splashCtrlPath, "json!" + metadataPath, "json!" + layoutPath], function(Splash, meta, layout){
         Splash(meta.logo).then(function($splash){
             jQuery(function ($) {
                 var $root = $('.root'),
                     $sectionsContainer = $(".root .sections-container"),
-                    sGuid = 0,
                     minStart = Date.now() + config.minSplashTime,
                     scrollToSection = function(id){
                         var $el = $sectionsContainer.find("[data-section-id='" + id + "']"),
@@ -52,8 +57,8 @@ require([
                         var nextId = i < layout.sections.length - 1 ? i + 1 : false,
                             scrollNext = nextId ? scrollToSection.bind(null, nextId) : function(){};
                         return when.promise(function(resolve, reject){
-                            var ctrlPath = config.ctrlPath + section.controller + '/main.js',
-                                contentPath = config.contentPath + section.content + '.md';
+                            var ctrlPath = ctrlBase + section.controller + '/main',
+                                contentPath = contentBase + section.content + '.md';
                             localRequire([ctrlPath], function(Controller){
                                 Controller(contentPath, meta, scrollNext).then(resolve, reject);
                             }, reject);
@@ -62,7 +67,7 @@ require([
                         });
                     }),
                     pNav = when.promise(function(resolve, reject){
-                        var ctrlPath = config.ctrlPath + 'nav/main.js';
+                        var ctrlPath = ctrlBase + 'nav/main';
                         localRequire([ctrlPath], function(NavController){
                             NavController(layout.sections, meta.logo, meta.title, scrollToSection).then(resolve, reject);
                         }, reject);
